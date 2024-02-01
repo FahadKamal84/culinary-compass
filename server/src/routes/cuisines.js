576c5c2e81581a5fed05cc1ca9dd3b08488c6104
Fahad Kamal <fahadkamal@hotmail.com>
2024-02-01 12:14:17 -0500

    move all cuisines route to cuisines route  file

--- server/src/routes/cuisines.js
+++ server/src/routes/cuisines.js
@@ -2,11 +2,24 @@ const express = require("express");
 const router = express.Router();
 const cuisinesQueries = require("../db/queries/cuisines");
 
+
 router.get("/", (req, res) => {
   cuisinesQueries
     .getAllCuisines()
     .then((data) => {
       res.status(200).json(data.cuisines);
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err.message });
+    });
+});
+
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  cuisinesQueries
+    .findCuisines(id)
+    .then((recipes) => {
+      res.status(200).json(recipes);
     })
     .catch((err) => {
       res.status(500).json({ error: err.message });
